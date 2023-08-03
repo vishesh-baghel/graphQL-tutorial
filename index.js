7ffa00a33ccf8cdceea8ec7a59a1b8c0424c3c0c
vishesh-baghel <visheshbaghel99@gmail.com>
2023-08-03 20:58:59 +0530

    learned basic of queries for related data

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import db from "./_db.js";
+import { typeDefs } from "./schema.js";
 
-const server = new ApolloServer({});
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((author) => author.author_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((author) => author.author_id === parent.author_id);
+    },
+    game(parent) {
+      return db.games.find((review) => review.id === parent.game_id);
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
